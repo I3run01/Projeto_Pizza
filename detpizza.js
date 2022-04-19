b9e5c3e7e4b914c78459276789c8a59a3b84593a
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-04-19 08:55:17 -0300

    commit

--- detpizza.js
+++ detpizza.js
@@ -1,7 +1,23 @@
 function detpizza(indice) {
+    document.querySelector('#detpizza').style.display = 'block'
 
     let obj = pizzas[indice]
-    document.querySelector('#detpizza').style.display = 'block'
-    document.querySelector('#DetPizzaDiv').innerHTML = obj.nome
+    document.querySelector('#detpizzaNome').innerHTML = obj.nome
+    document.querySelector('#detpizzaimg').src = obj.foto
+    document.querySelector('#detpizzaDescricao').innerHTML = obj.descriao
+    document.querySelector('#detpizzapreco').innerHTML = `R$ ${obj.preco}`
+}
 
+function colocarPizza() {
+    document.querySelector('#detpizzasQuantidades').innerHTML = 'teste'
+ 
+}
+
+function tirarPizza(){
+    document.querySelector('#detpizzasQuantidades').innerHTML = 'teste'
+    
+}
+
+function CarcelarPizza() {
+    document.querySelector('#detpizza').style.display = 'none'
 }
