91356658ffd4400bcb566873c69fabd63c624e42
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-04-20 10:12:28 -0300

    commit

--- Aside.js
+++ Aside.js
@@ -4,12 +4,14 @@ for (let i of pizzas) {
     if(i.quantidade > 0) {
         TextoListaPizza +=
             `
-            <p>${i.foto}</p>
-            <p>${i.nome}</p>
-            <div>
-                <div>+</div>
-                <div>un</div>
-                <div>-</div>
+            <div >
+                <p>${i.foto}</p>
+                <p>${i.nome}</p>
+                <div>
+                    <div>+</div>
+                    <div>un</div>
+                    <div>-</div>
+                </div>
             </div>
             `
     }
