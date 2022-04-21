9f733ad5238a56ad973ff9e34dd4d77f2af0fbc1
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-04-21 08:39:22 -0300

    commit

--- Aside.js
+++ Aside.js
@@ -1,25 +1,59 @@
 function AsideFunction() {
+
     let TextoListaPizza = ''
+    let cont = 0
     for (let i of pizzas) {  
         if(i.quantidade > 0) {
             TextoListaPizza +=
                 `
                 <div >
-                    <p>${i.foto}</p>
+                    <img src="${i.foto}"></p>
                     <p>${i.nome}</p>
                     <div>
-                        <div>+</div>
-                        <div>un</div>
-                        <div>-</div>
+                        <div onclick="tirarPizzaAside(${cont})">-</div>
+                        <div id="btnAside">${i.quantidade}</div>
+                        <div onclick="colocarPizzaAside(${cont})">+</div>
                     </div>
                 </div>
                 `
         }
+        cont++
     }
-
     document.querySelector('#lpizzas').innerHTML = TextoListaPizza
+    
+    CalculoAside()
+
+    
 }
 
+function CalculoAside() {
+    let soma = pizzas.reduce((soma, obj) => soma += obj.preco*obj.quantidade, 0)
+
+    let subtotal = document.querySelector('#subtotal').innerHTML = soma.toFixed(2)
+    let desconto = document.querySelector('#desconto').innerHTML = (soma*0.10).toFixed(2)
+    document.querySelector('#ValorTotalAside').innerHTML = (subtotal - desconto).toFixed(2)
+
+}
+
+function colocarPizzaAside(indice) {
+    let obj = pizzas[indice]
+    obj.AdicionarPizza()
+    document.querySelector('#btnAside').innerHTML = obj.quantidade
+
+    CalculoAside()
+
+    
+   
+}
+
+function tirarPizzaAside(indice) {
+    let obj = pizzas[indice]
+    obj.DiminuirPizza()
+    document.querySelector('#btnAside').innerHTML = obj.quantidade
+
+    CalculoAside()
+ 
+}
 
 
 
